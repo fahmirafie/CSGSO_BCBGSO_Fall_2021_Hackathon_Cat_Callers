ad462d51583fc6c58f54cbb55161df4d2a7eba15
Fahmi Rafie <20251573+fahmirafie@users.noreply.github.com>
2021-11-13 17:47:56 -0600

    Final

--- CatBuddy/CatBuddy.ino
+++ CatBuddy/CatBuddy.ino
@@ -1,75 +1,45 @@
 #include <Servo.h>
+ 
 #define CAMERA_ELEVATION 550             //height of camera relative to ground, dm
 #define CAMERA_ZONE_DISTANCE 75          //distance from the active zone to the camera on the y axis, dm
 #define ZONE_WIDTH 2                     //width of active zone, m
 #define ZONE_HEIGHT 7                    //height of active zone, m
-#define SERVO_DELAY 20                   //delay between signals to motor, ms
-#define TERMINATOR_CHAR '|'              //terminator for serial inputs, char
+#define SERVO_DELAY 250                  //delay between signals to motor, ms
 #define BAUD 9600 
-
-Servo servoHorizontal;
-Servo servoVertical;
-
-
-int angleHorizontal, angleVertical;
-int shPin = 5, svPin=4;
-
-void moveToHorizontal(int angle)
-{
-  servoHorizontal.write(angle);
-  angleHorizontal = angle;
-}
-
-void moveToVertical(int angle)
-{
-  servoVertical.write(angle);
-  angleVertical = angle;
-}
-
+ 
+#define H_PIN 12
+#define V_PIN 13
+int x, y, horizontalAngle, verticalAngle;
+Servo horizontalMotor, verticalMotor;
+ 
+/* init motors and serial communication with GPU*/
 void setup()
 {
-  // init servo motors
-//  angleHorizontal = 0;
-//  servoHorizontal.attach(shPin); // pwm pin 5
-//  servoHorizontal.write(angleHorizontal);
-//
-//  angleVertical = 0;
-//  servoVertical.attach(svPin); // pwm pin 4
-//  servoVertical.write(angleVertical);
-//  
-  //start up nvidia apu
-    // start serial port at 9600 bps:
- Serial.begin(BAUD);
-
-  while (!Serial) {
-       // wait for serial port to connect.
-  }
+    horizontalMotor.attach(H_PIN);
+    verticalMotor.attach(V_PIN);
+    horizontalMotor.write(0);
+    Serial.begin(9600);
+    while (! Serial);
 }
-
+ 
 void loop()
 {
+    /* get alerted of new position */
+    if(Serial.available() > 0)
+    {
+      horizontalAngle += Serial.parseInt();
+      verticalAngle  += Serial.parseInt();
+    }
 
-  if (Serial.available() > 0) {
-        String serialInput = Serial.readStringUntil(TERMINATOR_CHAR);
-        String ackMsg = "Recieved: " + serialInput + "\n";
-        Serial.print(ackMsg);
-        // parse string to set horizontal and vertical angle 
-  }
-  
- //moveToHorizontal(angleHorizontal);
- // moveToVertical(angleVertical);
-  
-//  angleHorizontal += 5;
-//  angleVertical +=5;
-//
-//  if(angleHorizontal == 180) angleHorizontal = 0;
-//
-//  if(angleVertical == 180) angleVertical = 0;
-  // real code starts here
-  //get all object detections, previously filtered out to only cats
-  //from data, get target cat
-  //get coordinates on screen of target cat's center
-  //get location of laser on screen
-
-  delay(SERVO_DELAY);
+    //get cat detection from jetson
+    //get coordinates on screen of target cat's center
+    //get location of laser on screen
+    //if cat is near location, pick random location
+    //calculate angle for new location
+    //send function to move to new angle
+    
+    /* update motor angles to get to or stay in desired position */
+    horizontalMotor.write(horizontalAngle);
+    verticalMotor.write(verticalAngle);
+    delay(SERVO_DELAY);
 }
